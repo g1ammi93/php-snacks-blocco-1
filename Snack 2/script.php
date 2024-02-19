9f990d3be012cb4115bb8ac027c9a0296930584b
g1ammi93 <giammariacarelli93@hotmail.it>
2024-02-19 19:46:49 +0000

    Added if for form access

---
+++ Snack 2/script.php
@@ -0,0 +1,33 @@
+<?php
+
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 2</title>
+</head>
+
+
+<body>
+
+<?php
+    if(isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])) {
+        if (strlen($name) > 3 && filter_var($mail, FILTER_VALIDATE_EMAIL) && is_numeric($age)) {
+            echo "<p>Accesso riuscito</p>";
+        } else {
+            echo "<p>Accesso negato</p>";
+        }
+    } if(empty($name) || empty($mail) || empty($age)) {
+        echo "<p>Per favore inserisci i campi correttamente</p>";
+    }
+?>
+
+</body>
+</html>
