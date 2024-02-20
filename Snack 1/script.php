5d760a6da7584a1902ba45c784884befe0c427c6
g1ammi93 <giammariacarelli93@hotmail.it>
2024-02-20 08:58:18 +0000

    Edit Array form

--- Snack 1/script.php
+++ Snack 1/script.php
@@ -1,11 +1,36 @@
 <?php
 
 $matches = [
-    'Olimpia Milano - Cantù | 55-60',
-    'Dinamo Sassari - Derthona | 62-79',
-    'Cremona - Napoli | 90-83',
-    'Brescia - Reggiana | 86-63',
-    'Virtus Bologna - Scafati | 94-67'
+    [
+        'home_team' => 'Olimpia Milano',
+        'away_team' => 'Cantù',
+        'home_point' => '55',
+        'away_point' => '60',
+    ],
+    [
+        'home_team' => 'Dinamo Sassari',
+        'away_team' => 'Derthona',
+        'home_point' => '62',
+        'away_point' => '79',
+    ],
+    [
+        'home_team' => 'Brescia',
+        'away_team' => 'Reggiana',
+        'home_point' => '86',
+        'away_point' => '63',
+    ],
+    [
+        'home_team' => 'Cremona',
+        'away_team' => 'Napoli',
+        'home_point' => '90',
+        'away_point' => '83',
+    ],
+    [
+        'home_team' => 'Virtus Bologna',
+        'away_team' => 'Scafati',
+        'home_point' => '94',
+        'away_point' => '67',
+    ],
 ];
 
 ?>
@@ -23,7 +48,7 @@ $matches = [
     <h1> Partite Giornata 1</h1>
     <ul>
         <?php for($i = 0; $i < count($matches); $i++) { ?>
-        <li> <?php echo $matches[$i] ?></li>
+        <li> <?php echo "{$matches[$i]['home_team']} - {$matches[$i]['away_team']}   |" ?> <?php echo "{$matches[$i]['home_point']}-{$matches[$i]['away_point']}" ?> </li>
         
          <?php } ?>
     </ul>
